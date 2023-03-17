ced9b7bf9067f6770ce4cb65976b958852e9b991
behoon <syauqeesy@gmail.com>
2023-03-17 19:25:13 +0700

    add personal assignment

--- algorithm-programming/personal-assignment/tp1-w3-s5-r2/2602298361_A2.c
+++ algorithm-programming/personal-assignment/tp1-w3-s5-r2/2602298361_A2.c
@@ -1,10 +1,11 @@
 /*
  *  Dalam contoh dibawah ini saya mendeklarasikan variable `char name[255]`.
  *  Saya ingin mengisi variable name tersebut menggunakan inputan dari console menggunakan scanf_s.
- *  Saat menggunakan function scanf_s saya memasukan dua buah argumen yaitu
- *  formattingnya dan tujuan alamat variable untuk menyimpan string hasil dari inputan menggunakan simbol `&`.
- *  Dalam contoh ini scanf_s akan menggunakan address dari variable name untuk dimasukan string dari inputan.
- *  Jika kita memasukan variable name tanpa simbol `&` didepan kita akan memasukan isi dari variable `name` yaitu string kosong.
+ *  Saat menggunakan function scanf_s saya memasukan dua buah argumen yaitu formattingnya
+ *  dan tujuan alamat variable untuk menyimpan string hasil dari inputan menggunakan simbol `&`.
+ *  Dalam contoh ini scanf_s membutuhkan address dari variable name untuk dimasukan string dari inputan.
+ *  scanf_s akan memasukan value ke dalam memory sesuai dengan alamat yang kita berikan.
+ *  Jika kita memasukan variable name tanpa simbol `&` didepan, kita akan memasukan isi dari variable `name` yaitu string kosong.
  *  Jadi itulah contoh penggunaan pointer dalam bahasa C, yaitu memasukan alamat dari variable ke function scanf_s
  *  untuk nanti dimasukan value dari hasil inputan console.
  */
