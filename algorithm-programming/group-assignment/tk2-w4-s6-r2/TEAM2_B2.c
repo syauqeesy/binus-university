ebf275eb7a00d27d65113e356f58b4652a98d954
behoon <syauqeesy@gmail.com>
2023-03-24 14:06:54 +0700

    style

--- algorithm-programming/group-assignment/tk2-w4-s6-r2/TEAM2_B2.c
+++ algorithm-programming/group-assignment/tk2-w4-s6-r2/TEAM2_B2.c
@@ -10,13 +10,16 @@ int main() {
   scanf("%f", &celsius);
 
   fahrenheit = (celsius * 9/5) + 32;
-  printf("%.2f celsius = %.2f fahrenheit\n", celsius, fahrenheit); // menggunakan %.2f agar hanya mengambil 2 angka setelah koma
+  printf("%.2f celsius = %.2f fahrenheit", celsius, fahrenheit); // menggunakan %.2f agar hanya mengambil 2 angka setelah koma
+  printf("\n");
 
   reamur = (celsius * 4/5);
-  printf("%.2f celsius = %.2f reamur\n", celsius, reamur); // menggunakan %.2f agar hanya mengambil 2 angka setelah koma
+  printf("%.2f celsius = %.2f reamur", celsius, reamur); // menggunakan %.2f agar hanya mengambil 2 angka setelah koma
+  printf("\n");
 
   kelvin = celsius + 273;
-  printf("%.2f celsius = %.2f kelvin\n", celsius, kelvin); // menggunakan %.2f agar hanya mengambil 2 angka setelah koma
+  printf("%.2f celsius = %.2f kelvin", celsius, kelvin); // menggunakan %.2f agar hanya mengambil 2 angka setelah koma
+  printf("\n");
 
   return 0;
 }
